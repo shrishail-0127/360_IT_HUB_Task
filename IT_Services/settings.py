d97b87440acf9be95be0cf17d4ccff18e881744e
shrishail-0127 <shriiipj420@gmail.com>
2024-09-03 19:57:22 +0530

    created home view

--- IT_Services/settings.py
+++ IT_Services/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Email settings for Gmail SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pythondeveloper0127@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = 'Python@0127'  # Replace with your App Password
+
+
 
 # Application definition
 
@@ -37,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "service",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +65,7 @@ ROOT_URLCONF = "IT_Services.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR,'service/tamplates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
